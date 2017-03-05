45df25ab596881ae3e3c3093aaea25e20a403e7c
slombardo <slombardo@users.noreply.github.com>
2017-03-05 12:38:33 -0500

    Update YouTube Loop.js

--- Scriptlets/YouTube Loop.js
+++ Scriptlets/YouTube Loop.js
@@ -14,46 +14,49 @@ javascript:
 // 5.   Enjoy endlessly :)
 /**************************************************************************/
 
-var playerElement = document.getElementById('player-api');
-var player = window.yt.player.getPlayerByElement(playerElement);
-var loopTime = 0;
-var doLoop = false;
+if(!document.getElementById('controlFrame')) 
+{
+    var playerElement = document.getElementById('player-api');
+    var player = window.yt.player.getPlayerByElement(playerElement);
+    var loopTime = 0;
+    var doLoop = false;
 
-var controlFrame = document.createElement("span");
-controlFrame.style.border = "1px solid #000000";
-controlFrame.style.padding = "5px";
-controlFrame.style.height = "30px";
-controlFrame.style.margin = "5px";
+    var controlFrame = document.createElement("span");
+    controlFrame.id = "controlFrame";
+    controlFrame.style.border = "1px solid #000000";
+    controlFrame.style.padding = "5px";
+    controlFrame.style.height = "30px";
+    controlFrame.style.margin = "5px";
 
-var loopAt = getSpan("No loop.");
-loopAt.style.width = "80px";
-loopAt.style.border = "1px solid #000000";
+    var loopAt = getSpan("No loop.");
+    loopAt.style.width = "80px";
+    loopAt.style.border = "1px solid #000000";
 
 
-var setPosition = getSpanButton("Loop at current time");
-setPosition.style.width = "120px";
-setPosition.style.clear = "both";
-setPosition.onclick = function() {
-    loopTime = player.getCurrentTime();
-    loopAt.innerHTML = loopTime;
-    player.seekTo(0);
-    player.playVideo();
-    doLoop = true;
-    setTimeout(loop, 1000);
-};
+    var setPosition = getSpanButton("Loop at current time");
+    setPosition.style.width = "120px";
+    setPosition.style.clear = "both";
+    setPosition.onclick = function() {
+        loopTime = player.getCurrentTime();
+        loopAt.innerHTML = loopTime;
+        player.seekTo(0);
+        player.playVideo();
+        doLoop = true;
+        setTimeout(loop, 1000);
+    };
 
-var clear = getSpanButton("clear");
-clear.onclick = function() {
-    doLoop = false;
-    loopAt.innerHTML = "No loop.";
-};
+    var clear = getSpanButton("clear");
+    clear.onclick = function() {
+        doLoop = false;
+        loopAt.innerHTML = "No loop.";
+    };
 
-var afterThisGuy = document.getElementById('player-messages');
-insertAfter(controlFrame, afterThisGuy.firstChild);
-controlFrame.appendChild(setPosition);
-controlFrame.appendChild(loopAt);
-controlFrame.appendChild(clear);
-
+    var afterThisGuy = document.getElementById('player-messages');
+    insertAfter(controlFrame, afterThisGuy.firstChild);
+    controlFrame.appendChild(setPosition);
+    controlFrame.appendChild(loopAt);
+    controlFrame.appendChild(clear);
+}
 
 function insertAfter(newNode, referenceNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
